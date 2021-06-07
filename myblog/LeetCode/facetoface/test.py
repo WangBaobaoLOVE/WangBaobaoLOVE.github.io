719aea12d245c60e88fb0e26f054d718e55eb5c6
WangBaobaoLOVE <wbb392797665@sina.com>
2021-06-07 21:36:21 +0800

    update leetcode

--- myblog/LeetCode/facetoface/test.py
+++ myblog/LeetCode/facetoface/test.py
@@ -1,4 +1,28 @@
-s = [[9,-8,1,3,-2],[-3,7,6,-2,4],[6,-4,-4,8,-7]]
+class StreamRank:
 
-print(s[0][1:3])
-print([s1[0] for s1 in s])
+    def __init__(self):
+        self.list = []
+
+
+    def track(self, x: int) -> None:
+        self.list.append(x)
+        self.list.sort(reverse=True)
+
+
+    def getRankOfNumber(self, x: int) -> int:
+        self.track(x)
+        index = self.list.index(x)
+        if index == len(self.list)-1:
+            self.list.pop(index)
+            return 0
+        num = len(self.list)-self.list.index(x)-1
+        self.list.pop(index)
+        return num
+
+s = StreamRank()
+s.track(4)
+s.track(3)
+s.track(5)
+s.getRankOfNumber(8)
+s.track(3)
+s.getRankOfNumber(2)
